78a4eeabe5976fddbe2bf33bc7da5bfeaf93e298
Tony <toscarreks@gmail.com>
2018-10-06 21:16:40 -0700

    renamed TESSTEST to img2mat, wrapping it in a function

--- js/img2mat.js
+++ js/img2mat.js
@@ -1,25 +1,27 @@
-var newImg = new Image;
-var tess_result;
-var dimension = 3;
-var confidence_threshold = 55;
+// Takes an image, attempts to grab a matrix from it
+function imageToMatrix(img, dimension){
 
-console.log("BEGINNING TEST");
+var newImg = img;
+var dim = dimension;
+var confidence_threshold = 55;
 
 newImg.onload = function () {
     Tesseract.recognize(newImg)
         .then(function (result) {
-            console.log(result)
+
+            //console.log(result)
             var t_m = [];
 
             // find nums line by line
-            for (i = 0; i < dimension; i++) {
+            for (i = 0; i < dim
+                 ; i++) {
                 var line = result.lines[i];
                 console.log(line);
                 t_m[i] = [];
 
                 // iterate through the symbols.
                 // add them if their conf is > 66
-                for (j = 0; j < dimension; j++) {
+                for (j = 0; j < dim; j++) {
                     if (line.symbols[j.toString()].confidence > confidence_threshold) {
                         t_m[i].push(parseFloat(line.symbols[j.toString()].text));
                     }
@@ -30,21 +32,18 @@ newImg.onload = function () {
             t_m.forEach(function (row) {
                 console.log(row);
             })
-        
+
             var t_m_ops = rref(t_m);
             var BIGM = rref_parser(t_m, t_m_ops);
-        
+
             BIGM.forEach(function (row) {
                 console.log(row);
             })
+
+            return t_m
         })
 };
 
-newImg.src = 'matrix_test1.jpeg';
+//newImg.src = 'matrix_test1.jpeg';
 
-
-var testM = [
-    [1, 2, 3],
-    [1, 1, 1],
-    [4, 5, 6]
-];
+}
