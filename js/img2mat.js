6c92c1a232dd29c3acba815b9ec32c8112fdb0e2
Tony <toscarreks@gmail.com>
2018-10-07 20:18:12 -0700

    steps for testing TessJob

--- js/img2mat.js
+++ js/img2mat.js
@@ -12,16 +12,24 @@ function parseTessJob(job, dim) {
     for (i = 0; i < dim; i++) {
         var line = job.lines[i];
         t_m[i] = [];
+        for(let j = 0; j < dim; j++){t_m[i][j] = 0;} //initialized to all zero
 
         // iterate through the symbols.
         // add them if their conf is > 66
-        let count = 0;
-        let j = 0;
+        /*
+            1. Omit non-digit symbols
+            2. Sort by confidence
+            3. Grab (up to) top 3
+            4. Find relative positions, insert into most relative column
+        */
+
+        let count = 0;  // t_m[i][j]
+        let j = 0;      // More of an iterator
 
         while (count < dim) {
 
             if (line.symbols[j.toString()].confidence > confidence_threshold) {
-                t_m[i].push(parseFloat(line.symbols[j.toString()].text));
+                t_m[i][count] = parseFloat(line.symbols[j.toString()].text));
                 count++;
             }
 
